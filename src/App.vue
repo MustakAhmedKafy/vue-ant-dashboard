7cadc401ba62a0b8e918e3c01596491d5801d822
Mustak Ahmed Kafy <43313297+MustakAhmedKafy@users.noreply.github.com>
2023-12-06 22:10:48 +0600

    layout add

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Layout from './components/layout/MainLayout.vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+</script>
+
+<template>
+  <Layout v-if="route.path != '/login'"></Layout>
+</template>
