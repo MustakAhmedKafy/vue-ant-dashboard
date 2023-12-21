4bcf91ee9a73c24f4b689e898144e600e59dee39
Mustak Ahmed Kafy <43313297+MustakAhmedKafy@users.noreply.github.com>
2023-12-22 00:16:57 +0600

    sider menu change

--- src/components/layout/SideBar.vue
+++ src/components/layout/SideBar.vue
@@ -1,47 +1,69 @@
 <script setup>
-import { TeamOutlined, PieChartOutlined, DesktopOutlined, UserOutlined, FileOutlined   } from '@ant-design/icons-vue';
-import { ref } from 'vue';
-const collapsed = ref(false);
-const selectedKeys = ref(['1']);
+import {
+  TableOutlined,
+  BarsOutlined,
+  ShoppingCartOutlined,
+  SettingOutlined,
+  FileOutlined,
+  MoneyCollectOutlined
+} from '@ant-design/icons-vue'
+import { ref } from 'vue'
+const collapsed = ref(false)
+const selectedKeys = ref(['1'])
+const logo = 'logo.png'
 </script>
 
 <template>
-   <a-layout-sider v-model:collapsed="collapsed" collapsible>
-      <div class="logo" />
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-        <a-menu-item key="1">
-          <pie-chart-outlined />
-          <span>Option 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <desktop-outlined />
-          <span>Option 2</span>
-        </a-menu-item>
-        <a-sub-menu key="sub1">
-          <template #title>
-            <span>
-              <user-outlined />
-              <span>User</span>
-            </span>
-          </template>
-          <a-menu-item key="3">Tom</a-menu-item>
-          <a-menu-item key="4">Bill</a-menu-item>
-          <a-menu-item key="5">Alex</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span>
-              <team-outlined />
-              <span>Team</span>
-            </span>
-          </template>
-          <a-menu-item key="6">Team 1</a-menu-item>
-          <a-menu-item key="8">Team 2</a-menu-item>
-        </a-sub-menu>
-        <a-menu-item key="9">
-          <file-outlined />
-          <span>File</span>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
+  <a-layout-sider v-model:collapsed="collapsed" collapsible>
+    <div class="logo">
+      <img :src="logo" alt="" />
+    </div>
+    <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+      <a-menu-item key="1">
+        <BarsOutlined />
+        <span>Dashboard</span>
+      </a-menu-item>
+      <a-menu-item key="2">
+        <ShoppingCartOutlined />
+        <span>Product</span>
+      </a-menu-item>
+      <a-sub-menu key="sub1">
+        <template #title>
+          <span>
+            <SettingOutlined />
+            <span>Configuration</span>
+          </span>
+        </template>
+        <a-menu-item key="3">Branch</a-menu-item>
+        <a-menu-item key="4">Create Menu</a-menu-item>
+        <a-menu-item key="5">Item</a-menu-item>
+        <a-menu-item key="6">Discounts</a-menu-item>
+        <a-menu-item key="7">Spalash Screen Image</a-menu-item>
+        <a-menu-item key="8">Add Product Property</a-menu-item>
+      </a-sub-menu>
+
+      <a-menu-item key="9">
+        <file-outlined />
+        <span>File</span>
+      </a-menu-item>
+      <a-menu-item key="9">
+        <MoneyCollectOutlined />
+        <span>Billing</span>
+      </a-menu-item>
+      <a-menu-item key="10">
+        <TableOutlined />
+        <span>Table</span>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-sider>
 </template>
+
+<style scoped>
+.logo {
+  text-align: center;
+  padding: 10px 0px;
+}
+.logo img {
+  width: 80px;
+}
+</style>
--- src/components/layout/TopHeader.vue
+++ src/components/layout/TopHeader.vue
@@ -1,6 +1,12 @@
 <script setup>
 import { ref } from 'vue'
-import { CaretDownOutlined, LoginOutlined } from '@ant-design/icons-vue'
+import {
+  CaretDownOutlined,
+  LoginOutlined,
+  ExpandOutlined,
+  UserOutlined
+} from '@ant-design/icons-vue'
+const adminPic = 'admin.jpg'
 const value = ref('')
 const isFullScreen = ref(false)
 const toggleFullScreen = () => {
@@ -47,41 +53,20 @@ const toggleFullScreen = () => {
     </a-menu>
 
     <a-space>
-      <a-menu
-       
-        style=""
+      <a-menu style="">
+        <a-button
+          @click="toggleFullScreen"
+          style="border: 0px; display: flex; align-items: center; box-shadow: none; font-size: 20px"
         >
-        <a-button  @click="toggleFullScreen" style="border: 0px; display: flex; align-items: center; margin-right: 10px; box-shadow: none;">
-          <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          fill="currentColor"
-          class="bi bi-fullscreen"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5M.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5"
-          /></svg>
+          <ExpandOutlined />
         </a-button>
-        </a-menu>
+      </a-menu>
       <a-menu>
         <a-dropdown class="admin-dropdown" placement="bottom">
           <template #overlay>
             <a-menu style="margin-top: -8px">
               <a-menu-item key="ac" style="padding-right: 30px; padding-bottom: 10px">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-person"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z"
-                  />
-                </svg>
+                <UserOutlined />
                 <span style="padding-left: 8px" class="gray-menu-item">Account</span>
               </a-menu-item>
               <a-divider class="divider" />
@@ -111,7 +96,8 @@ const toggleFullScreen = () => {
             </a-menu>
           </template>
           <div>
-            <a-avatar focus :size="44"> </a-avatar>
+            <img class="avatar" :src="adminPic" alt="admin" />
+            <!-- <a-avatar focus :size="44"> </a-avatar> -->
             <span class="gray-menu-item" style="margin-left: 8px"
               >Mustak <CaretDownOutlined
             /></span>
@@ -122,6 +108,13 @@ const toggleFullScreen = () => {
   </a-layout-header>
 </template>
 <style scoped>
+
+.avatar {
+  vertical-align: middle;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
 .ant-layout-header {
   padding-inline: 60px !important;
 }
